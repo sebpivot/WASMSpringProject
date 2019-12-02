3c85c4a559fa2814bc7e6c6c7824e379a1169bbe
sebpivot <seb.pivot@live.fr>
2019-12-02 15:25:07 +0100

    Creation of the new project

---
+++ src/main/java/com/adaming/wasmspringproject/WasmspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.adaming.wasmspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasmspringprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasmspringprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adaming/wasmspringproject/WasmspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaming.wasmspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasmspringprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
